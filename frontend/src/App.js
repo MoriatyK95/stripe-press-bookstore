c76d49c9d2ecf200de11f48e8c7af4a063957dc6
H.K. Zeng <hkzeng@HKs-MacBook-Pro.local>
2025-01-26 15:43:31 +0800

    Moving fetch pub key logic to CheckoutForm component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,29 +7,7 @@ import Success from './components/Success';
 
 // Main App component
 const App = () => {
-  const [publishableKey, setPublishableKey] = useState('');
 
-  useEffect(() => {
-    // Fetch the publishable key from the backend
-    fetch('http://127.0.0.1:5000/api/config')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        if (data.publishableKey) {
-          setPublishableKey(data.publishableKey);
-          console.log('Fetched publishable key:', data.publishableKey);
-        } else {
-          throw new Error('Publishable key not found');
-        }
-      })
-      .catch(error => {
-        console.error('Error fetching publishable key:', error);
-      });
-  }, []);
 
   return (
     <Router>
--- frontend/src/components/CheckoutForm.js
+++ frontend/src/components/CheckoutForm.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './CheckoutForm.css';
 
 // CheckoutForm component
 const CheckoutForm = ({ book }) => {
+
+
+  const [publishableKey, setPublishableKey] = useState('');
+
+  useEffect(() => {
+    // Fetch the publishable key from the backend
+    fetch('http://127.0.0.1:5000/api/config')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.publishableKey) {
+          setPublishableKey(data.publishableKey);
+          console.log('Fetched publishable key:', data.publishableKey);
+        } else {
+          throw new Error('Publishable key not found');
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching publishable key:', error);
+      });
+  }, []);
+
   return (
     <div className="checkout-form">
       <form>
